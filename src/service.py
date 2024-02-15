6574de0a6821c08a7e432419c91bd570e17b3da3
tm kartikey <ikartik27@gmail.com>
2024-02-15 22:54:44 +0530

    cannot create service

--- src/service.py
+++ src/service.py
@@ -1,7 +1,7 @@
 import bentoml
 
 from bentoml.io import NumpyNdarray
-my_best_model = bentoml.models.import_model('artifacts\my_best_model.bentomodel')
+my_best_model = bentoml.models.get('my_best_model:latest')
 my_best_model_runner = my_best_model.to_runner()
 
 
@@ -10,3 +10,4 @@ svc = bentoml.Service('my_model', runners=[my_best_model_runner])
 @svc.api(input=bentoml.io.NumpyNdarray(),output=bentoml.io.NumpyNdarray(),route='/beleth')
 def predict(input_arr):
     return my_best_model_runner.predict.run(input_arr)
+
