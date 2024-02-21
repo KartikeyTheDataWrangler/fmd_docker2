257d9fff768f442b47f2ab90b19569681f0c993f
tm kartikey <ikartik27@gmail.com>
2024-02-22 00:23:12 +0530

    docker and flask app inititated

---
+++ artifacts/main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+# Fixing file path separator
+with open(file=r'current_best_model\model.pkl', mode="rb") as file:
+    current_model = pickle.load(file)
+
+app = Flask(__name__)
+
+# Define an endpoint for rendering the HTML form
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Define an endpoint for making predictions
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the data from the request
+        data = request.form.to_dict()
+        input_data = np.array([float(data[key]) for key in sorted(data.keys())]).reshape(1, -1)
+
+        # Make prediction using the loaded model
+        prediction = current_model.predict(input_data)
+
+        # Return the prediction to the HTML template
+        return render_template('index.html', prediction=prediction[0])
+    except Exception as e:
+        return render_template('index.html', error=str(e))
+
+if __name__ == '__main__':
+    # Run the Flask app on port 5000
+    app.run(port=5000)
